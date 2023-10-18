500be0b911b48517402d790b370aeb8e6d57304f
Gyanaranjan das <143919182+bigbook77744@users.noreply.github.com>
2023-10-18 10:49:39 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
-  printf("hello world");
+  int a,b;
+  printf("enter a:");
+  scanf("%d",&a);
+  printf("enter b:");
+  scanf("%d",&b);
+  int sum=a+b;
+  printf("%d",sum);
   return 0; 
+
 }
